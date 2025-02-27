2e4dd8ae317b0908fff7e847f4084bc34e5b8723
Scorcherfjk <franjavikale@gmail.com>
2025-02-27 16:49:42 -0500

    feat: update Tailwind CSS to version 4.0.9 and add Tailwind configuration file

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      colors: {
+        black: "#2F2E41",
+      },
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+};
