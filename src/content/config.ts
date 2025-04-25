2db81bd2c2dc33d2a402e3e515dab03bb3aedded
Scorcherfjk <franjavikale@gmail.com>
2025-04-25 11:14:36 -0500

    feat: add legal documents including liability waiver, privacy policy, and terms and conditions

--- src/content/config.ts
+++ src/content/config.ts
@@ -13,6 +13,17 @@ const blogCollection = defineCollection({
     }),
 });
 
+const legalCollection = defineCollection({
+  type: "content",
+  schema: () =>
+    z.object({
+      title: z.string(),
+      description: z.string(),
+      date: z.date(),
+    }),
+});
+
 export const collections = {
   blog: blogCollection,
+  legal: legalCollection,
 };
